a49560fc46b1394985d06897e017f3a41b0da581
hariBoWoo <85086591+hariBoWoo@users.noreply.github.com>
2021-06-07 17:42:54 +0900

    June 7
    modal, JS class

---
+++ 4Cloud04/hardcode.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#btn0").click(function(){
+    alert("Text: " + $("objName0").text());
+  });
+});
+
+$(document).ready(function(){
+    $("#btn1").click(function(){
+      alert("Text: " + $("objName1").text());
+    });
+  });
+  
