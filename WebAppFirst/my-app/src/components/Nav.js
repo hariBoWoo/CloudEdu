72e28cbb77a1838c9fbfd50fcc413f18a926818d
hariBoWoo <85086591+hariBoWoo@users.noreply.github.com>
2021-06-09 17:47:35 +0900

    June-9
    add READ ME

--- WebAppFirst/my-app/src/components/Nav.js
+++ WebAppFirst/my-app/src/components/Nav.js
@@ -11,6 +11,13 @@ export default function Nav(){
         '인스타그램',
     ]); // Json type
 
+
+    const [name, setName] = useState('mike');
+
+    function showText(e){
+        console.log(e.target.value);
+    }
+
     return(
       <Fragment>
         <nav>
@@ -30,6 +37,25 @@ export default function Nav(){
                 <li><a href="3.html">JavaScript</a></li> */}
             </ul>
         </nav>
+
+        <div>
+          <h1>HERE CHANGE! : {name}</h1>
+          {/* 인라인 방식(아래)을 써야할때가 있으므로 함수 만들어서 쓰는 법과 같이 알아두자 */}
+          <button onClick={()=>{
+            let newName = name === 'mike' ? 'jane' : 'mike';
+            setName(newName);
+          }}>click Me!</button>
+
+                <hr ></hr>
+
+          <div>
+            <h6> 아래 박스에 적히는 것을 실시간으로 인식가능해서 (지금은 콘솔로 확인가능) 
+              예를들어 회원가입시 쓰면안되는 특수기호를 바로바로 확인해서 경고를 발생할수있음! </h6>
+            <input type="text" onChange={showText}/>
+          </div>
+        </div>
+        <hr ></hr>
+
       </Fragment>
     );
   }
---
+++ WebAppFirst/readME.txt
@@ -0,0 +1,4 @@
+cd my-app | npm start 로 시작 돌린다
+
+노드js 깔고, 리액트 깔고, npm 깔렸는지 확인하고 > npm --version
+Material-ui 에서 쓰기위해 npm 으로 깔기 > npm install @material-ui/core
